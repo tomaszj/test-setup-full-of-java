b6659cf0a5d3792fe9a98a83c16b54eaab1c1abc
Tomasz Janeczko <t.janeczko@kainos.com>
2014-10-12 22:27:17 +0100

    Updated Selenium tests.
    * Changed the main module back to appDev, so the application uses mocks
      by default.

--- SeleniumTests/src/test/java/org/tomaszjaneczko/testpoc/selenium_tests/BusinessesTests.java
+++ SeleniumTests/src/test/java/org/tomaszjaneczko/testpoc/selenium_tests/BusinessesTests.java
@@ -32,5 +32,31 @@ public class BusinessesTests extends BaseTest {
         assertThat("Business Id contains correct ID", fullBusinessId.contains("#1"), is(true));
     }
 
+    @Test
+    public void itShouldAddBusiness() {
+        webDriver.get(baseUrl + "/businesses");
 
+        WebElement nameElement = webDriver.findElement(By.className("business-name-field"));
+        nameElement.sendKeys("New name");
+
+        WebElement submitElement = webDriver.findElement(By.className("business-add-button"));
+        submitElement.click();
+
+        WebElement alertElement = webDriver.findElement(By.className("alert"));
+        assertThat("Business Id contains correct ID",
+                alertElement.getText().contains("Business added successfully"), is(true));
+
+    }
+
+    @Test
+    public void itShouldRemoveBusiness() {
+        webDriver.get(baseUrl + "/businesses/2");
+
+        WebElement deleteElement = webDriver.findElement(By.className("business-delete-button"));
+        deleteElement.click();
+
+        WebElement alertElement = webDriver.findElement(By.className("alert"));
+        assertThat("Business removed message present",
+                alertElement.getText().contains("removed"), is(true));
+    }
 }
